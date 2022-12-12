0417cda3384b6fab3c81015fade6e2272300a7d0
swoonning <cmtugade@hotmail.com>
2022-12-12 16:18:50 -0600

    Got the Stripe session working

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,25 @@
+class CheckoutController < ApplicationController
+  def create
+    character = Character.find(params[:id])
+    @session = Stripe::Checkout::Session.create({
+      success_url: root_url,
+      cancel_url: root_url,
+      line_items: [{
+        # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price_data: {
+          currency: "cad",
+          unit_amount: character.price,
+          product_data: {
+            name: character.name,
+          }
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+
+    })
+    respond_to do |format|
+      format.js
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :characters, only: %i[index show sort recent hype]
   resources :elements, only: %i[index]
 
+
   get '/sort', to: "characters#sort"
   get '/recent', to: "characters#recent"
   get '/hype', to: "characters#hype"
   get '/search', to: "characters#search"
+  post "checkout/create", to: "checkout#create"
 
   # Defines the root path route ("/")
   root "characters#index"
