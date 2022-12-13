e5def55b812d6643b6c7a692b4bfb04ddf8a8f9f
swoonning <cmtugade@hotmail.com>
2022-12-13 09:56:51 -0600

    Taxes are calculated based on province

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,8 +3,12 @@ class CheckoutController < ApplicationController
     character = Character.find(params[:id])
     @session = Stripe::Checkout::Session.create({
       customer: current_user.stripe_customer_id,
+      customer_update: {
+        address: 'auto',
+      },
       success_url: root_url,
       cancel_url: root_url,
+      payment_method_types: ['card'],
       line_items: [{
         # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
         price_data: {
@@ -12,13 +16,19 @@ class CheckoutController < ApplicationController
           unit_amount: character.price,
           product_data: {
             name: character.name,
-          }
+          },
+          tax_behavior: 'exclusive',
         },
         quantity: 1,
       }],
+      automatic_tax: {
+        enabled: true
+      },
       mode: 'payment',
+      billing_address_collection: 'required',
 
-    })
+    }
+  )
     respond_to do |format|
       format.js
     end
